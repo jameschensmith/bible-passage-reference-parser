1fee33ea5dad8154263e07cc4f7c3d62184f51ea
James Chen-Smith <jameschensmith@gmail.com>
2021-02-23 17:03:15 -0600

    feat: convert bcv_utils to TS
    Converts bcv_utils to TypeScript.
    Updates compile script to run tsc.
    Updates tsconfig to compile the correct files in the correct folder.

---
+++ src/core/bcv_utils.ts
@@ -0,0 +1,7 @@
+export const bcv_utils = {
+	// Make a shallow clone of an object. Nested objects are referenced, not cloned.
+	shallow_clone: <T extends {}>(obj: T) => ({ ...obj }),
+
+	// Make a shallow clone of an array. Nested objects are referenced, not cloned.
+	shallow_clone_array: <T>(arr: T[]) => [...arr]
+};
