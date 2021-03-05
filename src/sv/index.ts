954d23709caddc38e1e1f6dba19b29eb9505bd9a
James Chen-Smith <jameschensmith@gmail.com>
2021-03-05 13:00:06 -0600

    chore: clean up lang bcv_parser export

--- src/sv/index.ts
+++ src/sv/index.ts
@@ -1,13 +1,11 @@
-import { bcv_parser } from "../core";
+import { bcv_parser as base_bcv_parser } from "../core";
 const grammar = require("./grammar");
 
 require("./regexps");
 require("./translations");
 
-export = {
-	bcv_parser: class extends bcv_parser {
-		constructor() {
-			super(grammar);
-		}
+export class bcv_parser extends base_bcv_parser {
+	constructor() {
+		super(grammar);
 	}
-};
+}
